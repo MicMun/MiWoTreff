12434e41568370830395c705f1eeba64dcdae07c
MicMun <MicMunze@gmail.com>
2013-01-18 19:31:38 +0100

    Delete Item added with undo possibility.

--- src/de/micmun/android/miwotreff/MainActivity.java
+++ src/de/micmun/android/miwotreff/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
 import android.util.Log;
@@ -24,172 +25,229 @@ import de.micmun.android.miwotreff.utils.DbAdapter;
 import de.micmun.android.miwotreff.utils.LoaderListener;
 import de.micmun.android.miwotreff.utils.ProgramLoader;
 import de.micmun.android.miwotreff.utils.SpecialCursorAdapter;
+import de.micmun.android.miwotreff.utils.UndoBarController;
 
 /**
  * Main Activity of the app.
- *
+ * 
  * @author MicMun
  * @version 1.0, 13.01.2013
  */
-public class MainActivity extends ListActivity implements LoaderListener{
-	private static final String TAG = "MiWoTreff";
-	private DbAdapter mDbHelper; // Database Helper
-	
-	private MenuItem btnRefresh = null;
+public class MainActivity extends ListActivity implements LoaderListener,
+         UndoBarController.UndoListener
+{
+   private static final String TAG = "MiWoTreff";
+   private DbAdapter mDbHelper; // Database Helper
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+   private MenuItem btnRefresh = null;
 
-		mDbHelper = new DbAdapter(this);
-		try {
-			mDbHelper.open();
-		} catch (SQLException s) {
-			Log.e(TAG, s.getLocalizedMessage());
-			AppMsg.makeText(this, R.string.db_open_error, 
-			                AppMsg.STYLE_ALERT).show();
-			return;
-		}
-		fillData();
-		registerForContextMenu(getListView());
-	}
+   private UndoBarController mUndoBarController;
 
-	/**
-	 * @see android.app.ListActivity#onDestroy()
-	 */
-	@Override
-	public void onDestroy() {
-		mDbHelper.close();
-		super.onDestroy();
-	}
+   private String tmpDelDatum;
+   private String tmpDelThema;
+   private String tmpDelPerson;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		btnRefresh = menu.findItem(R.id.menu_refresh);
-		// BackupActionProvider for backup/restore
-		MenuItem bi = menu.findItem(R.id.menu_export);
-		BackupActionProvider bap = new BackupActionProvider(this);
-		bap.setActivity(this);
-		bi.setActionProvider(bap);
-		return true;
-	}
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
 
-	/**
-	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
-	 */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId())  {
-			case R.id.menu_search:
-				onSearchRequested();
-				return true;
-			case R.id.menu_refresh:
-				ProgramLoader pl = new ProgramLoader(this, btnRefresh);
-				pl.addLoaderListener(this);
-				pl.execute(new Void[] {});
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
-	
-	/**
-    * @see android.app.Activity#onCreateContextMenu(android.view.ContextMenu, android.view.View, android.view.ContextMenu.ContextMenuInfo)
+      mDbHelper = new DbAdapter(this);
+      try {
+         mDbHelper.open();
+      } catch (SQLException s) {
+         Log.e(TAG, s.getLocalizedMessage());
+         AppMsg.makeText(this, R.string.db_open_error, AppMsg.STYLE_ALERT)
+                  .show();
+         return;
+      }
+      fillData();
+      registerForContextMenu(getListView());
+
+      mUndoBarController = new UndoBarController(findViewById(R.id.undobar),
+                                                 this);
+   }
+
+   /**
+    * @see android.app.ListActivity#onDestroy()
     */
    @Override
-   public void onCreateContextMenu(ContextMenu menu, View v, 
+   public void onDestroy() {
+      mDbHelper.close();
+      super.onDestroy();
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.activity_main, menu);
+      btnRefresh = menu.findItem(R.id.menu_refresh);
+      // BackupActionProvider for backup/restore
+      MenuItem bi = menu.findItem(R.id.menu_export);
+      BackupActionProvider bap = new BackupActionProvider(this);
+      bap.setActivity(this);
+      bi.setActionProvider(bap);
+      return true;
+   }
+
+   /**
+    * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+    */
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+         case R.id.menu_search:
+            onSearchRequested();
+            return true;
+         case R.id.menu_refresh:
+            ProgramLoader pl = new ProgramLoader(this, btnRefresh);
+            pl.addLoaderListener(this);
+            pl.execute(new Void[] {});
+            return true;
+         default:
+            return super.onOptionsItemSelected(item);
+      }
+   }
+
+   /**
+    * @see android.app.Activity#onCreateContextMenu(android.view.ContextMenu,
+    *      android.view.View, android.view.ContextMenu.ContextMenuInfo)
+    */
+   @Override
+   public void onCreateContextMenu(ContextMenu menu, View v,
                                    ContextMenu.ContextMenuInfo menuInfo) {
       super.onCreateContextMenu(menu, v, menuInfo);
       MenuInflater inflater = getMenuInflater();
       inflater.inflate(R.menu.context_menu, menu);
    }
-   
+
    /**
     * @see android.app.Activity#onContextItemSelected(android.view.MenuItem)
     */
    @Override
    public boolean onContextItemSelected(MenuItem item) {
-      AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
-      
+      AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+
       switch (item.getItemId()) {
          case R.id.addToCal: // Add to google calendar
             add2Call(info);
             return true;
          case R.id.delItem: // Delete Item
-            //delItem(info);
+            delItem(info);
             return true;
          default:
             return super.onContextItemSelected(item);
       }
    }
 
-	/**
-	 * @see de.micmun.android.miwotreff.utils.LoaderListener#update()
-	 */
-	@Override
-	public void update() {
-		fillData();
-	}
+   /**
+    * @see de.micmun.android.miwotreff.utils.LoaderListener#update()
+    */
+   @Override
+   public void update() {
+      fillData();
+   }
 
-	/**
-	 * Reads the data from database and fill the list.
-	 */
-	private void fillData() {
-		Cursor entryCursor = mDbHelper.fetchAllEntries(null);
-		SpecialCursorAdapter adapter = new SpecialCursorAdapter(this,entryCursor);
-		setListAdapter(adapter);
-	}
+   /**
+    * Reads the data from database and fill the list.
+    */
+   private void fillData() {
+      Cursor entryCursor = mDbHelper.fetchAllEntries(null);
+      SpecialCursorAdapter adapter = new SpecialCursorAdapter(this, entryCursor);
+      setListAdapter(adapter);
+   }
 
-	/**
-	 * Adds the entry to the calendar.
-	 * 
-	 * @param  info
-	 *         Info about the entry.
-	 */
-	private void add2Call(AdapterContextMenuInfo info) {
-		Cursor c = mDbHelper.fetchEntry(info.id);
-		// Date and time of the calendar entry
-		long d = c.getLong(1);
-		GregorianCalendar start = new GregorianCalendar();
-		GregorianCalendar end = new GregorianCalendar();
-		start.setTimeInMillis(d);
-		start.set(GregorianCalendar.HOUR_OF_DAY, 19);
-		start.set(GregorianCalendar.MINUTE, 30);
-		end.setTimeInMillis(d);
-		end.set(GregorianCalendar.HOUR_OF_DAY, 21);
+   /**
+    * Adds the entry to the calendar.
+    * 
+    * @param info
+    *           Info about the entry.
+    */
+   private void add2Call(AdapterContextMenuInfo info) {
+      Cursor c = mDbHelper.fetchEntry(info.id);
+      // Date and time of the calendar entry
+      long d = c.getLong(1);
+      GregorianCalendar start = new GregorianCalendar();
+      GregorianCalendar end = new GregorianCalendar();
+      start.setTimeInMillis(d);
+      start.set(GregorianCalendar.HOUR_OF_DAY, 19);
+      start.set(GregorianCalendar.MINUTE, 30);
+      end.setTimeInMillis(d);
+      end.set(GregorianCalendar.HOUR_OF_DAY, 21);
 
-		// title
-		String title = getResources().getString(R.string.cal_prefix) + " " +  
-					c.getString(2);
+      // title
+      String title = getResources().getString(R.string.cal_prefix) + " "
+               + c.getString(2);
 
-		// location
-		String loc = getResources().getString(R.string.cal_loc);
+      // location
+      String loc = getResources().getString(R.string.cal_loc);
 
-		// Calendar: Insert per Intent
-		Intent intent = new Intent(Intent.ACTION_INSERT)
-		.setData(Events.CONTENT_URI)
-		.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, start.getTimeInMillis())
-		.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end.getTimeInMillis())
-		.putExtra(Events.TITLE, title)
-		.putExtra(Events.EVENT_LOCATION, loc)
-		.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
-		startActivity(intent);
-	}
+      // Calendar: Insert per Intent
+      Intent intent = new Intent(Intent.ACTION_INSERT)
+               .setData(Events.CONTENT_URI)
+               .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                         start.getTimeInMillis())
+               .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                         end.getTimeInMillis()).putExtra(Events.TITLE, title)
+               .putExtra(Events.EVENT_LOCATION, loc)
+               .putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
+      startActivity(intent);
+   }
 
-	/**
-	 * Deletes an item from database and list.
-	 * 
-	 * @param  info
-	 *         Info about the entry.
-	 */
-//	private void delItem(AdapterContextMenuInfo info) {
-//		if (!mDbHelper.deleteEntry(info.id)) {
-//			Log.e(TAG, "Can't delete Entry with ID = " + info.id);
-//			return;
-//		}
-//		fillData();
-//	}
+   /**
+    * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
+    */
+   @Override
+   protected void onSaveInstanceState(Bundle outState) {
+      super.onSaveInstanceState(outState);
+      mUndoBarController.onSaveInstanceState(outState);
+   }
+
+   /**
+    * @see android.app.ListActivity#onRestoreInstanceState(android.os.Bundle)
+    */
+   @Override
+   protected void onRestoreInstanceState(Bundle savedInstanceState) {
+      super.onRestoreInstanceState(savedInstanceState);
+      mUndoBarController.onRestoreInstanceState(savedInstanceState);
+   }
+
+   /**
+    * Deletes an item from database and list.
+    * 
+    * @param info
+    *           Info about the entry.
+    */
+   private void delItem(AdapterContextMenuInfo info) {
+      /* Store temporarily */
+      Cursor c = mDbHelper.fetchEntry(info.id);
+      tmpDelDatum = DbAdapter.getDateString(c.getLong(1));
+      tmpDelThema = c.getString(2);
+      tmpDelPerson = c.getString(3);
+      c.close();
+
+      if (!mDbHelper.deleteEntry(info.id)) {
+         String msg = String
+                  .format(getResources().getString(R.string.error_delItem),
+                          info.id);
+         Log.e(TAG, msg);
+         AppMsg.makeText(this, msg, AppMsg.STYLE_ALERT).show();
+         return;
+      } else { // Successfully deleted
+         mUndoBarController
+                  .showUndoBar(false,
+                               getString(R.string.undobar_entry_deleted), null);
+         fillData();
+      }
+   }
+
+   /**
+    * @see de.micmun.android.miwotreff.utils.UndoBarController.UndoListener#onUndo(android.os.Parcelable)
+    */
+   @Override
+   public void onUndo(Parcelable token) {
+      mDbHelper.createEntry(DbAdapter.getDateFromString(tmpDelDatum),
+                            tmpDelThema, tmpDelPerson);
+      fillData();
+   }
 }
